35b11dbc7b56332bcf3d1c2d8b2ab90f72057507
ClaudWatari <claudwatari95@gmail.com>
2019-11-10 18:08:28 +0300

    addRouting

---
+++ routes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+import authAdmin from '../auth/auth';
+
+import userController from './ctrl';
+
+const router = express.Router();
+// const app = express();
+
+router.post('/api/v1/auth/create-user', authAdmin, userController.createUser);
+
+export default router;
